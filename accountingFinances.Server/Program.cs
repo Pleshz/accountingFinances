0cc3a2ab1ad133bd38f12690454c3e2ce8f5f2fe
pleshd <pleskovd96@gmail.com>
2025-03-28 18:36:37 +0500

    В Program.cs добавил подключение к PostgreSQL. в appsettings.json добавил строку подключение к БД. Плешков

--- accountingFinances.Server/Program.cs
+++ accountingFinances.Server/Program.cs
@@ -1,9 +1,10 @@
+using accountingFinances.Server.Classes.Common;
+using Microsoft.EntityFrameworkCore;
+
 var builder = WebApplication.CreateBuilder(args);
 
-// Add services to the container.
-
+builder.Services.AddDbContext<DataBaseContext>(options => options.UseNpgsql(builder.Configuration.GetConnectionString("DefaultConnection")));
 builder.Services.AddControllers();
-// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
 
@@ -12,7 +13,6 @@ var app = builder.Build();
 app.UseDefaultFiles();
 app.UseStaticFiles();
 
-// Configure the HTTP request pipeline.
 if (app.Environment.IsDevelopment())
 {
     app.UseSwagger();
