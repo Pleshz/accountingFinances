4fc40c0ee3fed08e1a79f9554c3af2e8950e8d55
pleshd <pleskovd96@gmail.com>
2025-03-28 18:16:14 +0500

    Создал модель операций. Плешков

---
+++ accountingFinances.Server/Models/Operation.cs
@@ -0,0 +1,13 @@
+﻿namespace accountingFinances.Server.Models
+{
+    public class Operation
+    {
+        public int Id { get; set; }
+        public string Description { get; set; }
+        public decimal Amount { get; set; }
+        public DateTime Date { get; set; }
+
+        public int CategoryId { get; set; }
+        public Category Category { get; set; }
+    }
+}
