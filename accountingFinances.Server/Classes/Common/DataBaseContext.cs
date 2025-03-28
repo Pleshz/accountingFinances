2ff4f8589fdde90fa632edc16ca0681063bf15fe
pleshd <pleskovd96@gmail.com>
2025-03-28 18:23:32 +0500

    Создал класс DataBaseContext для работы с базой данных. Плешков

---
+++ accountingFinances.Server/Classes/Common/DataBaseContext.cs
@@ -0,0 +1,12 @@
+﻿using accountingFinances.Server.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace accountingFinances.Server.Classes.Common
+{
+    public class DataBaseContext : DbContext
+    {
+        public DataBaseContext(DbContextOptions<DataBaseContext> options) : base(options) { }
+        public DbSet<Category> Categories { get; set; }
+        public DbSet<Operation> Operations { get; set; }
+    }
+}
